bcad8f04acb01177d6060da965989b284f920073
DaviDemarqui <davidemarqui@genesyssoft.com.br>
2022-12-02 10:50:57 -0300

    prototipo do supabase

---
+++ src/app/supabase.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import {
+  AuthChangeEvent,
+  AuthSession,
+  createClient,
+  Session,
+  SupabaseClient,
+  User,
+} from '@supabase/supabase-js'
+import { environment } from "src/environments/environment";
+// import { Database } from 'src/schema';
+// Dps ver o porque da linha acima???
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SupabaseService {
+  private supabase: SupabaseClient
+  _session: AuthSession | null = null
+
+  constructor() {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey)
+  }
+
+  get session() {
+    this.supabase.auth.getSession().then(({ data }) => {
+      this._session = data.session
+    })
+    return this._session
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://kjulugmdepadnaoksrus.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqdWx1Z21kZXBhZG5hb2tzcnVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk5ODY4NzcsImV4cCI6MTk4NTU2Mjg3N30.TXCt02aKhfizDM099Cr5KKtFEzjPtZDQoz-UP6pCQfw'
 };
 
 /*
