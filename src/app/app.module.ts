bc9235a7b96d887512872250c471cda1e14f00dc
FernandoPestillo <fernandopestillo@gmail.com>
2022-12-02 12:01:34 -0300

    Login criado e pasta components movida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FooterComponent } from 'src/components/footer/footer.component';
-import { HeaderComponent } from 'src/components/header/header.component';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
+import { HeaderComponent } from 'src/app/components/header/header.component';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent, 
-    FooterComponent,
+    FooterComponent, LoginComponent,
   ],
   imports: [
     BrowserModule
