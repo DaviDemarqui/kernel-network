1e2421ceb2fbcbeb40f203474e4f49c2bdbf0c94
DaviDemarqui <davidemarqui@genesyssoft.com.br>
2022-12-05 11:40:00 -0300

    enviroments variables fixes for vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://kjulugmdepadnaoksrus.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqdWx1Z21kZXBhZG5hb2tzcnVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk5ODY4NzcsImV4cCI6MTk4NTU2Mjg3N30.TXCt02aKhfizDM099Cr5KKtFEzjPtZDQoz-UP6pCQfw'
 };
