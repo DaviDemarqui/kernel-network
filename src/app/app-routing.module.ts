32481da3f6db0c8d97d5a66a2719731b8b6e47f3
FernandoPestillo <fernandopestillo@gmail.com>
2022-12-06 11:41:46 -0300

    rota home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
@@ -7,8 +8,9 @@ const routes: Routes = [
     {
       path: 'login', component: LoginComponent },
     {
-      path: '', component: HomeComponent ,
-    }
+      path: '', component: HeroComponent },
+    {
+      path: 'home', component: HomeComponent,}
 
 ];
 
