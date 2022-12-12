148ed9bc20b30031674160030809ad10f7532b0f
DaviDemarqui <davidemarqui@genesyssoft.com.br>
2022-12-12 09:50:01 -0300

    criação do post view component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HeroComponent } from './components/hero/hero.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NewPostComponent } from './components/new-post/new-post.component';
+import { PostViewComponent } from './components/post-view/post-view.component';
 import { AuthGuard } from './guards/auth.guard';
 import { LoggedInAuthGuard } from './guards/loggedInAuth.guard';
 
@@ -19,6 +20,8 @@ const routes: Routes = [
       path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
     {
       path: 'new', component: NewPostComponent, canActivate: [AuthGuard]},
+    {
+      path: 'view', component: PostViewComponent, canActivate: [AuthGuard]},
 
 ];
 
