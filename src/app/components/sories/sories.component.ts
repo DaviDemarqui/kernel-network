5864f93a3e63ee7e2e73b62516db9d393c0ff3be
DaviDemarqui <davidemarqui@genesyssoft.com.br>
2024-01-10 17:48:11 -0300

    New stories component

---
+++ src/app/components/sories/sories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sories',
+  templateUrl: './sories.component.html',
+  styleUrls: ['./sories.component.css']
+})
+export class SoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
