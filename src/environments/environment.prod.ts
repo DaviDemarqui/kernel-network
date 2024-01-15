f4e6b1a71610df164be8fc9958fd5673b8149a50
DaviDemarqui <davidemarqui@genesyssoft.com.br>
2024-01-15 09:46:09 -0300

    Minor fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://kjulugmdepadnaoksrus.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqdWx1Z21kZXBhZG5hb2tzcnVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk5ODY4NzcsImV4cCI6MTk4NTU2Mjg3N30.TXCt02aKhfizDM099Cr5KKtFEzjPtZDQoz-UP6pCQfw'
+  supabaseUrl: 'https://iaqpwtoliaqcilistlau.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhcXB3dG9saWFxY2lsaXN0bGF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkzNTAxODIsImV4cCI6MjAwNDkyNjE4Mn0.kt9jDrcYpkfFr-O4k3r2_BcCy1U9jOYZUOIrphkgqfo'
 };
