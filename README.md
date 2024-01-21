620f907d0489b4306c4813fbdbe897563d33cb10
Davi Demarqui <davicoding@protonmail.com>
2024-01-21 02:21:43 +0000

    Removing old name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BeKim
+# Kernel
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.0.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/be-kim'),
+      dir: require('path').join(__dirname, './coverage/kernel'),
       subdir: '.',
       reporters: [
         { type: 'html' },
